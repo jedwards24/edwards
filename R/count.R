69a93a16a2e012c6e0f9c04d9907473e9b68e7f4
James <jamese1326@gmail.com>
2019-08-28 08:41:09 +0100

    Redo count_ functions to use non-tidyverse code

--- R/count.R
+++ R/count.R
@@ -6,44 +6,61 @@
 #'
 #' Returns a vector of the number or proportion of NAs of each variable in a data frame.
 #'
-#' @param tbl a data frame.
+#' @param df a data frame.
 #'
-#' @param prop=F Set to true to return the number of NAs as a proportion of rows, and count otherwise.
+#' @param prop By default the count os NAs is returned. Set to TRUE instead return a proportion.
+#' @param all By default variables with no NAs are omitted from the output. Set to TRUE to show all.
 #'
 #' @export
-count_nas <- function(tbl, prop=F) {
-  summarise_all(tbl, funs(sum(is.na(.)))) %>%
-    unlist %>%
-    `/`(., if(prop){dim(tbl)[1]}else{1})
+count_nas <- function(df, prop = FALSE, all = FALSE) {
+  if (!is.list(df)) {
+    stop("`df` must be a list.", call. = FALSE)
+  }
+  vals <- vapply(df, function(x) sum(is.na(x)), integer(1))
+  if (prop) {
+    vals <- vals / dim(df)[1]
+  }
+  if(max(vals) == 0){
+    cat("There are no NAs in the data.\n")
+  }else{
+    vals[vals > 0 | all]
+  }
 }
 
 #########################################################################################
 # count_unique: counts the number of unique values in a data frame by column.
 #########################################################################################
 #'
-#' Counts NAs in a data frame by column.
+#' Counts the number of unique values in a data frame by column.
 #'
 #' Returns a vector of the number of unique values of each variable in a data frame.
 #'
-#' @param tbl a data frame.
+#' @param df A data frame.
 #'
 #' @export
-count_unique <- function(tbl) {
-  summarise_all(tbl, funs(n_distinct(.))) %>% unlist
+count_unique <- function(df) {
+  if (!is.list(df)) {
+    stop("`df` must be a list.", call. = FALSE)
+  }
+  vapply(df, function(x) length(unique(x)), integer(1))
 }
 
 #########################################################################################
-# count_unique: counts the number of levels in a data frame by column.
+# count_levels: counts the total number of levels in a data frame by column.
 #########################################################################################
 #'
-#' count_nas
-#' Counts NAs in a data frame by column.
+#' Counts the number of levels in a data frame by column.
 #'
 #' Returns a vector of the number of levels of each variable in a data frame.
 #'
-#' @param tbl a data frame.
+#' @param df A data frame.
+#' @param all By default variables with no levels are omitted from the output. Set all=T to show all.
 #'
 #' @export
-count_levels <- function(tbl) {
-  summarise_all(tbl, funs(nlevels(.))) %>% unlist
+count_levels <- function(df, all = FALSE) {
+  if (!is.list(df)) {
+    stop("`df` must be a list.", call. = FALSE)
+  }
+  vals <- vapply(df, nlevels, integer(1))
+  vals[vals > 0 | all]
 }
