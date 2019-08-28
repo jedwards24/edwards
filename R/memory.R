f7b7adc924fa55936c4801d6baf373f263c04333
James <jamese1326@gmail.com>
2019-08-28 11:07:18 +0100

    Rewrite object_size_all() to use more base R functions

--- R/memory.R
+++ R/memory.R
@@ -1,12 +1,14 @@
 ############
-# object_size_all: returns table of sizes of all object in Mb, in order
+# object_size_all: returns table of sizes of all object in Mb, in order.
 ############
 #'
-#' object_size_all: returns table of sizes of all object in Mb, in order
+#' object_size_all: returns table of sizes of all object in Mb, in order.
 #'
-#' @return A data frame with two columns: object name and size in Mb.
+#' @return A tibble with two columns: object name and size in Mb.
 #' @export
-object_size_all <- function(){
-  Mb <- ls(envir=.GlobalEnv) %>% sapply(. %>% get %>% object.size %>% '/'(10^6))
-  return(tibble(object=names(Mb), Mb=Mb) %>% arrange(desc(Mb)))
+object_size_all2 <- function(){
+  names <- ls(envir = .GlobalEnv)
+  mb <- vapply(names, function(x) object.size(get(x, envir = .GlobalEnv)) / 10^6, numeric(1))
+  mb <- sort(mb, decreasing = TRUE)
+  tibble::tibble(object = names(mb), Mb = mb)
 }
