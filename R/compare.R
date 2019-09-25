369d7097b9fc72ee0aa4e85c466ed927edbb32bd
James <jamese1326@gmail.com>
2019-09-25 11:04:12 +0100

    Add compare_vecs() and move main part of compare_vars() to compare_vecs()
    More detailed comparison made of NA elements. No longer outputs mean differences.

--- R/compare.R
+++ R/compare.R
@@ -1,43 +1,78 @@
 #########################################################################################
-# compare_vars: Summarise pairwise element ordering of two variables in a dataframe.
+# compare_vars: Summarise pairwise element comparison of two variables in a dataframe.
 #########################################################################################
 #'
-#' Summarise pairwise element ordering of two variables in a dataframe.
+#' Summarise pairwise element comparison of two variables in a dataframe.
 #'
 #' Returns a table summarising the ordering of corresponding elements of two columns in a data frame.
-#' Also prints a comparison of the means of the vectors if numeric. If ordering is not appropriate for the
-#' variables (i.e. not dates or numeric) then pariwise equality will be summarised.
 #'
 #' @param df A data frame.
 #' @param var1,var2 The two columns to be compared, either string names or integer positions.
+#' @param simple Boolean. If TRUE then only checks for pairwise differences, not ordering.
 #'
 #' @export
-compare_vars <- function(df, var1, var2) {
+compare_vars <- function(df, var1, var2, simple = F) {
   vec1 <- df[, var1, drop=T]
   vec2 <- df[, var2, drop=T]
-  if(is.factor(vec1)) vec1 <- levels(vec1)[vec1]
-  if(is.factor(vec2)) vec2 <- levels(vec2)[vec2]
-  both_num <- is.numeric(vec1) && is.numeric(vec2)
-  both_date <- (lubridate::is.Date(vec1) && lubridate::is.Date(vec2)) ||
-    (lubridate::is.POSIXt(vec1) && lubridate::is.POSIXt(vec2))
   name1 <- if (is.numeric(var1)) names(df)[var1] else var1
-  name2 <- if (is.numeric(var1)) names(df)[var2] else var2
-  if(!(both_num || both_date)){
-    diff_all <- vec1 == vec2
-    diff <- diff_all[!is.na(diff_all)]
-    tibble(comparison = c(paste(name1, "=", name2), paste(name1, "!=", name2), "Missing"),
-           count = c(sum(diff), sum(!diff), length(diff_all) - length(diff)),
-           prop = count / length(diff_all)
-    )
-  }else{
-    diff_all <- vec1 - vec2
-    diff <- diff_all[!is.na(diff_all)]
-    cat(name1, "has mean", mean(vec1, na.rm = T), "and", name2, "has mean", mean(vec2, na.rm = T), "\n")
+  name2 <- if (is.numeric(var2)) names(df)[var2] else var2
+  print(names(vec1))
+  compare_vecs(vec1, vec2, names = c(name1, name2), simple = simple)
+}
 
-    cat("Mean difference (", name1, " - ", name2, ") is ", mean(diff_all, na.rm = T), "\n", sep = "")
-    tibble(comparison = c(paste(name1, ">", name2), paste(name1, "<", name2), paste(name1, "==", name2), "Missing"),
-           count = c(sum(diff > 0), sum(diff < 0), sum(diff == 0), length(diff_all) - length(diff)),
-           prop = count / length(diff_all)
-    )
+#########################################################################################
+# compare_vecs: Summarise pairwise element comparison of two vectors.
+#########################################################################################
+#'
+#' Summarise pairwise element comparison of two vectors.
+#'
+#' Returns a table summarising the ordering of corresponding elements of the two supplied vectors. Inputs can be
+#' factors but must be atomic and the same length.
+#'
+#' @param x,y The two vectors to be compared. Both vectors must be atomic and of the same length.
+#' @param names Optional length 2 string vector of names for each vector. Otherwise, "x" and "y" are used.
+#' @param simple Boolean. If TRUE then only checks for pairwise differences, not ordering.
+#'
+#' @export
+compare_vecs <- function(x, y, names = NULL, simple = F){
+  if (!(is.atomic(x) & is.atomic(y))){
+    stop("Both vectors must be atomic.")
   }
+  if (length(x) != length(y)){
+    stop("x and y must be the same length.")
+  }
+  if(is.factor(x)) x <- levels(x)[x]
+  if(is.factor(y)) y <- levels(y)[y]
+  if (is.null(names)) {
+    name1 <- "x"
+    name2 <- "y"
+  }else{
+    name1 <- names[1]
+    name2 <- names[2]
+  }
+  both_na <- is.na(x) & is.na(y)
+  equal <- sum(x == y, na.rm = T)
+  if(simple){
+    same = sum(both_na) + equal
+    return(tibble::tibble(comparison = c("Match", "Different"),
+                          count = c(same, length(x) - same),
+                          prop = count / length(x)
+    ))
+  }
+  tibble::tibble(comparison = c(paste(name1, "==", name2),
+                                paste(name1, ">", name2),
+                                paste(name1, "<", name2),
+                                "Both NA",
+                                paste(name1, "NA only"),
+                                paste(name2, "NA only")
+  ),
+  count = c(sum(equal),
+            sum(x > y, na.rm = T),
+            sum(x < y, na.rm = T),
+            sum(both_na),
+            sum(!both_na & is.na(x)),
+            sum(!both_na & is.na(y))
+  ),
+  prop = count / length(x)
+  )
 }
