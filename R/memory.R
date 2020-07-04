3a6153d5e4833e069f34293be69f96230929c9d1
James <jamese1326@gmail.com>
2020-07-04 20:33:07 +0100

    Fix namespace bug in object_size_all() and units

--- R/memory.R
+++ R/memory.R
@@ -2,15 +2,15 @@
 # object_size_all: Returns table of sizes of all object in Mb, in order.
 ############
 #'
-#' Get sizes of all objects in global environment
+#' Get sizes, in Mb, of all objects in global environment
 #'
-#' Returns table of sizes of all objects in the global environment in Mb, in decreasing order of size.
+#' Returns table of sizes of all objects in the global environment in Mb (1024^2 B), in decreasing order of size.
 #'
 #' @return A tibble with two columns: object name and size in Mb.
 #' @export
 object_size_all <- function(){
   names <- ls(envir = .GlobalEnv)
-  mb <- vapply(names, function(x) stats::object.size(get(x, envir = .GlobalEnv)) / 10^6, numeric(1))
+  mb <- vapply(names, function(x) utils::object.size(get(x, envir = .GlobalEnv)) / 1024^2, numeric(1))
   mb <- sort(mb, decreasing = TRUE)
   tibble::tibble(object = names(mb), Mb = mb)
 }
