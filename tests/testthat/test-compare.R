454a1f494eabf957e20da33a62572e3db76a7798
James Edwards <jamese1326@gmail.com>
2022-02-05 16:38:59 +0000

    Refactor find_similar() and add tests

--- tests/testthat/test-compare.R
+++ tests/testthat/test-compare.R
@@ -39,3 +39,40 @@ test_that("compare_sets() is correct", {
   expect_equal(tb1$count[2], sum(tb2$just_x))
   expect_equal(tb1$count[3], sum(tb2$just_y))
 })
+
+test_that("find_similar() is correct", {
+  dt <- tibble::tibble(a = 0:4,
+                        b = c(0L, 1L, 6L, 6L, NA),
+                        c = as.double(0:4),
+                        u = letters[1:5],
+                        v = c(NA, "b", "c", "f", NA),
+                        w = c("aa", "b", NA, "f", NA))
+  res_expect <- tibble::tribble(
+    ~var1, ~var2,      ~class, ~match, ~match_zero, ~both_na, ~na_1, ~na_2, ~diff,    ~prop_match_nz,
+    "a",   "b",   "integer",     2L,          1L,       0L,    0L,    1L,    2L,       1/3,
+    "u",   "v", "character",     2L,          0L,       0L,    0L,    2L,    1L,       2/3,
+    "u",   "w", "character",     1L,          0L,       0L,    0L,    2L,    2L,       1/3,
+    "v",   "w", "character",     2L,          0L,       1L,    1L,    1L,    0L,       1
+  )
+  dt2 <- tibble::tibble(b2 = c(0L, 1L, 6L, 6L, NA),
+                        c2 = as.double(0:4),
+                        v2 = c(NA, "b", "c", "f", NA))
+
+  res_expect2 <- tibble::tribble(
+    ~var1, ~var2,      ~class, ~match, ~match_zero, ~both_na, ~na_1, ~na_2, ~diff,    ~prop_match_nz,
+    "a",  "b2",   "integer",     2L,          1L,       0L,    0L,    1L,    2L,               1/3,
+    "b",  "b2",   "integer",     4L,          1L,       1L,    0L,    0L,    0L,                 1,
+    "c",  "c2",   "numeric",     5L,          1L,       0L,    0L,    0L,    0L,                 1,
+    "u",  "v2", "character",     2L,          0L,       0L,    0L,    2L,    1L,               2/3,
+    "v",  "v2", "character",     3L,          0L,       2L,    0L,    0L,    0L,                 1,
+    "w",  "v2", "character",     2L,          0L,       1L,    1L,    1L,    0L,                 1
+  )
+  expect_equal(res_expect, find_similar(dt))
+  expect_equal(res_expect2, find_similar(dt, dt2))
+  # test zero row inputs
+  expect_identical(dim(find_similar(data.frame())), c(0L, 10L))
+  dt3 <- tibble::tibble(a = integer(0), b = integer(0))
+  dt4 <- tibble::tibble(a = 1:3, b = letters[1:3])
+  expect_identical(dim(find_similar(dt3)), c(1L, 10L))
+  expect_identical(dim(find_similar(dt4)), c(0L, 10L))
+})
